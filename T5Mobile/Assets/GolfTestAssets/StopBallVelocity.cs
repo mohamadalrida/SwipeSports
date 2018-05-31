f95cc0effea8c184b6bb8495a9414800b322bf9d
AminBahsa <37161900+AminBahsa@users.noreply.github.com>
2018-05-31 12:38:32 +1000

    scripts added

---
+++ T5Mobile/Assets/GolfTestAssets/StopBallVelocity.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopBallVelocity : MonoBehaviour {
+
+    public Rigidbody2D rb;
+
+    public Vector2 stopSpeed;
+    private Vector2 currentBallSpeed;
+    private Vector2 newPosition;
+
+    
+
+    
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        currentBallSpeed = rb.velocity;
+
+        //stopSpeed = new Vector2((-1f, 1f), (-100f, 100f));
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        currentBallSpeed = rb.velocity;
+
+        Debug.Log("current velocity = " + rb.velocity);
+
+        if (Input.GetKeyDown("space"))
+        {
+            Debug.Log("Stopping Speed Reached ");
+
+            currentBallSpeed = new Vector2(0, 0);
+
+            Debug.Log("Ball Stopped ");
+        }
+
+        
+        if ( currentBallSpeed == stopSpeed)
+        {
+
+            Debug.Log("Stopping Speed Reached ");
+
+            currentBallSpeed = new Vector2(0, 0);
+
+            Debug.Log("Ball Stopped ");
+
+        }
+
+	}
+}
