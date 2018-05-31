9b09426e6c9f81e401a7e707cd7e2477dfc2a32e
AminBahsa <37161900+AminBahsa@users.noreply.github.com>
2018-05-31 10:37:22 +1000

    GolfTest Scene template added

---
+++ T5Mobile/Assets/GolfTestAssets/DragThrow.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragThrow : MonoBehaviour
+{
+
+    Vector2 startPos;
+    Vector2 endPos;
+    Vector2 direction;
+
+    float touchTimeStart;
+    float touchTimeFinish;
+    float timeInterval;
+
+    Rigidbody2D rb;
+
+    bool throwAllowed = true;
+
+    public float throwForce = 0.3f;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            touchTimeStart = Time.time;
+            startPos = Input.mousePosition;
+        }
+
+        if (Input.GetMouseButtonUp(0) && throwAllowed)
+        {
+            touchTimeFinish = Time.time;
+
+            timeInterval = touchTimeFinish - touchTimeStart;
+
+            endPos = Input.mousePosition;
+
+            direction = startPos - endPos;
+
+            rb.isKinematic = false;
+            rb.AddForce(-direction / timeInterval * throwForce);
+
+            throwAllowed = false;
+        }
+
+    }
+}
---
+++ T5Mobile/Assets/GolfTestAssets/WinLevel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinLevel : MonoBehaviour {
+
+    public GameObject Ball;
+    public GameObject winTitle;
+
+    
+    private void OnTriggerEnter2D(Collider2D Ball)
+    {
+        //winTitle.SetActive(true);
+
+        SceneManager.LoadScene("golftesting");
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+		
+	}
+}
