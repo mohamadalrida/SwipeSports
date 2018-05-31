319811b04b940134f00744a76741fcb64dbab759
mohamadalrida <36973080+mohamadalrida@users.noreply.github.com>
2018-05-31 10:01:03 +1000

    Polished gameplay

--- T5Mobile/Assets/Score.cs
+++ T5Mobile/Assets/Score.cs
@@ -7,16 +7,35 @@ public class Score : MonoBehaviour
 
     public Text scoreText;
     public int score;
+    public GameObject scorer;
+    public GameObject timerGO;
+    public GameObject startButton;
+    public float time;
+    public Text timer;
+    
 
     // Use this for initialization
     void Start()
     {
-
+        startButton.SetActive(false);
+        scoreText.enabled = true;
+        timerGO.SetActive(true);
+        time = 5;
     }
 
     // Update is called once per frame
     void Update()
     {
+        timer.text = "Time Left: " + time.ToString("f1");
+        time -= Time.deltaTime;
+        
+        if(time <= 0)
+        {
+            
+            timerGO.SetActive(false);
+            scorer.GetComponent<Score>().enabled = false;
+
+        }
 
         scoreText.text = "Score: " + score;
 
@@ -25,4 +44,5 @@ public class Score : MonoBehaviour
             score++;
         }
     }
+    
 }
