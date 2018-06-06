23a5fefa407f6ab6b9a4419dfa0c0c8fd67be833
mohamadalrida <36973080+mohamadalrida@users.noreply.github.com>
2018-06-06 14:13:42 +1000

    ScoreManager

---
+++ T5Mobile/Assets/Scripts/StrokeManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StrokeManager : MonoBehaviour {
+
+    public Text strokes;
+    public Text par;
+    public Text hole;
+    private int strokesNumber = 0;
+    public int parNumber;
+    public int holeNumber;
+
+
+	// Use this for initialization
+	void Start () {
+
+        strokes.text = "" + strokesNumber;
+        par.text = "" + parNumber;
+        hole.text = "" + holeNumber;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            strokesNumber++;
+            strokes.text = "" + strokesNumber;
+        }
+
+	}
+}
