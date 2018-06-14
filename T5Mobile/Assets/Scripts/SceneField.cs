f58519d9f9e43342dc67cafcac548347ba4e9c1f
mohamadalrida <36973080+mohamadalrida@users.noreply.github.com>
2018-06-14 11:44:55 +1000

    Added 2D Curved Colliders and First Golf Level

--- T5Mobile/Assets/Scripts/SceneField.cs
+++ T5Mobile/Assets/Scripts/SceneField.cs
@@ -6,10 +6,6 @@ using UnityEngine.SceneManagement;
 public class SceneField : MonoBehaviour
 {
 
-   
-
-    
-
     [SerializeField]
     private Object m_SceneAsset;
     [SerializeField]
@@ -18,13 +14,19 @@ public class SceneField : MonoBehaviour
     {
          get { return m_SceneName; }
     }
+
     // makes it work with the existing Unity methods (LoadLevel/LoadScene)
     public static implicit operator string(SceneField sceneField)
     {
         return sceneField.SceneName;
     }
 
-    public void ScreenSwitcher()
+    //public void ScreenSwitcher()
+    //{
+    //    SceneManager.LoadScene(m_SceneName);
+    //}
+
+    private void OnTriggerEnter2D(Collider2D Ball)
     {
         SceneManager.LoadScene(m_SceneName);
     }
