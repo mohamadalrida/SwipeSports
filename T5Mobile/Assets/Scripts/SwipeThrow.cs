644f3f802faffdab6eb23f0aee99795cab572d1e
AminBahsa <37161900+AminBahsa@users.noreply.github.com>
2018-06-28 15:51:34 +1000

    script change

--- T5Mobile/Assets/Scripts/SwipeThrow.cs
+++ T5Mobile/Assets/Scripts/SwipeThrow.cs
@@ -40,6 +40,8 @@ public class SwipeThrow : MonoBehaviour {
 
             if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended && throwAllowed)
             {
+                rb.gravityScale = 1.0f; // reset gravity
+
                 touchTimeFinish = Time.time;
 
                 timeInterval = touchTimeFinish - touchTimeStart;
