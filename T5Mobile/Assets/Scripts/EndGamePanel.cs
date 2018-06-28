b59ad78fa45a7e45adf7075c9e9382ee99d796b1
mohamadalrida <36973080+mohamadalrida@users.noreply.github.com>
2018-06-28 14:53:33 +1000

    Added TextMeshPro and Added Pause Menu

---
+++ T5Mobile/Assets/Scripts/EndGamePanel.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class EndGamePanel : MonoBehaviour {
+
+    public GameObject Pause;
+    public GameObject UnPause;
+
+    public TextMeshProUGUI levelNumber;
+    public TextMeshProUGUI strokeNumber;
+    public TextMeshProUGUI parNumber;
+
+    public TextMeshProUGUI levelNo;
+    public TextMeshProUGUI strokeNo;
+    public TextMeshProUGUI parNo;
+
+    public Canvas mainCanvas;
+    public Canvas endCanvas;
+
+    public GameObject mainMenuConfirmation;
+
+    public string nextLevel;
+
+
+
+	// Use this for initialization
+	void Start () {
+
+        Time.timeScale = 1;
+        levelNo.text = "Completed Level: " + levelNumber.text;
+        strokeNo.text = "in" + strokeNumber.text + "strokes.";
+        parNo.text = "Par: " + parNumber.text;
+        
+        mainCanvas.enabled = false;
+        endCanvas.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Continue()
+    {
+        SceneManager.LoadScene(nextLevel);
+    }
+
+    public void MenuConfirmation()
+    {
+        mainMenuConfirmation.SetActive(true);
+
+        //SceneManager.LoadScene("MainMenuSwipeSports");
+    }
+
+    public void BackToMenu()
+    {
+        mainMenuConfirmation.SetActive(false);
+    }
+
+    public void BackToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenuSwipeSports");
+    }
+
+    public void Pausing()
+    {
+        if (Time.timeScale == 1)
+        {
+            Time.timeScale = 0;
+            endCanvas.enabled = true;
+            mainCanvas.enabled = false;
+        }
+        
+    }
+
+    public void UnPausing()
+    {
+        if (Time.timeScale == 0)
+        {
+            mainCanvas.enabled = true;
+            endCanvas.enabled = false;
+            
+            Time.timeScale = 1;
+        }
+    }
+}
--- T5Mobile/Assets/Scripts/StrokeManager.cs
+++ T5Mobile/Assets/Scripts/StrokeManager.cs
@@ -2,23 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class StrokeManager : MonoBehaviour {
 
     public static int strokesNumber;
-    public Text strokes;
+    
 
-    public Text par;
-    public Text hole;
+    public TextMeshProUGUI strokess;
+    public TextMeshProUGUI par;
+    public TextMeshProUGUI hole;
+
     public int parNumber;
     public int holeNumber;
-
 
 	// Use this for initialization
 	void Start () {
 
         strokesNumber = 0;
-        strokes.text = "" + strokesNumber;
+        strokess.text = "" + strokesNumber;
+        
         par.text = "" + parNumber;
         hole.text = "" + holeNumber;
 
@@ -27,7 +30,7 @@ public class StrokeManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-        strokes.text = "" + strokesNumber;
+        strokess.text = "" + strokesNumber;
 
 	}
 }
