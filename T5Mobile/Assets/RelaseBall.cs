bc1b50e8f40fc451d02881b2577bf6ee4bed64aa
AminBahsa <37161900+AminBahsa@users.noreply.github.com>
2018-06-28 15:41:54 +1000

    push

---
+++ T5Mobile/Assets/RelaseBall.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RelaseBall : MonoBehaviour {
+
+    public GameObject theBall1;
+
+    private Rigidbody2D rb;
+
+    public float releaseSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+
+    private void OnTriggerStay2D(Collider2D theBall1)
+    {
+        Debug.Log("Release!!!");
+
+        theBall1.transform.parent = transform;
+
+        rb = theBall1.GetComponent<Rigidbody2D>();
+
+        theBall1.attachedRigidbody.AddForce(transform.forward * releaseSpeed);
+    }
+}
---
+++ T5Mobile/Assets/Scripts/CatapultLauncher.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatapultLauncher : MonoBehaviour {
+
+    private Animator myAnimator;
+
+    public GameObject Player;
+
+    public GameObject theBall;
+
+    public float launchTime;
+    public float timeToLaunch;
+
+    public float childTime;
+
+    // Use this for initialization
+    void Start ()
+    {
+        myAnimator = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            myAnimator.SetTrigger("Launch");
+            
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            Player.transform.parent = null;
+
+        }
+
+    }
+
+    private void OnTriggerStay2D(Collider2D Player)
+    {
+        Debug.Log("InCatapult");
+
+
+
+        if (launchTime <= timeToLaunch)
+        {
+            launchTime += Time.deltaTime;
+
+            
+        }
+
+        if (launchTime >= timeToLaunch)
+        {
+            Player.transform.parent = transform;
+
+            myAnimator.SetTrigger("Launch");
+
+            if (childTime <= 1)
+            {
+
+                 childTime += Time.deltaTime;
+
+            }
+
+            //if (childTime >= 0.25f)
+            //{
+            //    Player.transform.parent = null;
+
+            //    childTime = 0;
+
+            //    Debug.Log("WORK!!!!!!!!!!!!!");
+
+            //}
+
+            
+            launchTime = 0;
+        }
+
+        if (childTime >= 0.0168f)
+        {
+            Player.transform.parent = null;
+
+            childTime = 0;
+
+            //launchTime = 0;
+
+            Debug.Log("WORK!!!!!!!!!!!!!");
+
+        }
+
+
+    }
+}
