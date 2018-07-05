07527144ac53c610d6fc415e64a5755cadb2e6cb
mohamadalrida <36973080+mohamadalrida@users.noreply.github.com>
2018-07-05 10:21:29 +1000

    Base Canvas' Input

--- T5Mobile/Assets/Scripts/WinLevel.cs
+++ T5Mobile/Assets/Scripts/WinLevel.cs
@@ -60,6 +60,35 @@ public class WinLevel : MonoBehaviour
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
+
+
+        //Insantiate all the the scores
+        Level1 = "" + Strokes1;
+        Levels1.text = Level1;
+
+        Level2 = "" + Strokes2;
+        Levels2.text = Level2;
+
+        Level3 = "" + Strokes3;
+        Levels3.text = Level3;
+
+        Level4 = "" + Strokes4;
+        Levels4.text = Level4;
+
+        Level5 = "" + Strokes5;
+        Levels5.text = Level5;
+
+        Level6 = "" + Strokes6;
+        Levels6.text = Level6;
+
+        Level7 = "" + Strokes7;
+        Levels7.text = Level7;
+
+        Level8 = "" + Strokes8;
+        Levels8.text = Level8;
+
+        Level9 = "" + Strokes9;
+        Levels9.text = Level9;
     }
 
     private void OnTriggerEnter2D(Collider2D Ball)
@@ -91,41 +120,57 @@ public class WinLevel : MonoBehaviour
         if (levelNumber == 2)
         {
             Strokes2 = StrokeManager.strokesNumber;
+            Level2 = "" + Strokes2;
+            Levels2.text = Level2;
         }
 
         if (levelNumber == 3)
         {
             Strokes3 = StrokeManager.strokesNumber;
+            Level3 = "" + Strokes3;
+            Levels3.text = Level3;
         }
 
         if (levelNumber == 4)
         {
             Strokes4 = StrokeManager.strokesNumber;
+            Level4 = "" + Strokes4;
+            Levels4.text = Level4;
         }
 
         if (levelNumber == 5)
         {
             Strokes5 = StrokeManager.strokesNumber;
+            Level5 = "" + Strokes5;
+            Levels5.text = Level5;
         }
 
         if (levelNumber == 6)
         {
             Strokes6 = StrokeManager.strokesNumber;
+            Level6 = "" + Strokes6;
+            Levels6.text = Level6;
         }
 
         if (levelNumber == 7)
         {
             Strokes7 = StrokeManager.strokesNumber;
+            Level7 = "" + Strokes7;
+            Levels7.text = Level7;
         }
 
         if (levelNumber == 8)
         {
             Strokes8 = StrokeManager.strokesNumber;
+            Level8 = "" + Strokes8;
+            Levels8.text = Level8;
         }
 
         if (levelNumber == 9)
         {
             Strokes9 = StrokeManager.strokesNumber;
+            Level9 = "" + Strokes9;
+            Levels9.text = Level9;
         }
 
         // Play Audio of Clapping and winning
