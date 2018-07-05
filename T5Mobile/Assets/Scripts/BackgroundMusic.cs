1f236ffe1dba11d4059d33bd05655ebc57a1b4d5
AminBahsa <37161900+AminBahsa@users.noreply.github.com>
2018-07-05 11:12:20 +1000

    backgorund music added

---
+++ T5Mobile/Assets/Scripts/BackgroundMusic.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMusic : MonoBehaviour {
+
+    public bool turnMusicOn;
+
+    AudioSource audioSource;
+
+    private void Awake()
+    {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("Music");
+        if (objs.Length > 1)
+        
+            Destroy(this.gameObject);
+        
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+	}
+}
---
+++ T5Mobile/Assets/Scripts/TurnMusicOn.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnMusicOn : MonoBehaviour {
+
+    public GameObject backGroundMusic;
+
+	// Use this for initialization
+	void Start ()
+    {
+        backGroundMusic.SetActive(true);
+
+        Destroy(this.gameObject);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
