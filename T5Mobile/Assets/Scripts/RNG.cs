9c80e0d0a76388205e879e7e328a40ea9b0de34e
AminBahsa <37161900+AminBahsa@users.noreply.github.com>
2018-07-05 13:32:26 +1000

    RNG pick up

---
+++ T5Mobile/Assets/Scripts/RNG.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RNG : MonoBehaviour {
+
+    private int chance;
+
+    public int percentage;
+
+    public GameObject coin;
+
+	// Use this for initialization
+	void Start ()
+    {
+        chance = Random.Range(0, 100);
+
+        Debug.Log("RNG" + chance);
+
+        if (chance >= percentage)
+        {
+            Instantiate(coin);
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
