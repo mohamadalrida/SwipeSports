523ad797a7bf262df8d84b7eaed12999b85e103f
mohamadalrida <36973080+mohamadalrida@users.noreply.github.com>
2018-07-25 02:54:33 +1000

    Ads Implemented

--- T5Mobile/Assets/Scripts/ADMoney.cs
+++ T5Mobile/Assets/Scripts/ADMoney.cs
@@ -2,22 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Advertisements;
+using TMPro;
 
 public class ADMoney : MonoBehaviour {
 
     public static int AdMoney;
+    public TextMeshProUGUI adMoney;
+
 
 	// Use this for initialization
 	void Start () {
+        Advertisement.Initialize("2685945");
         AdMoney = PlayerPrefs.GetInt("AdMoney",AdMoney);
 	}
-	
-	//public void ShowAd()
-    //{
-    //    if (//Advertisement.IsReady())
-    //    {
-    //        //Advertisement.Show();
-    //    }
-    //}
+
+    private void Update()
+    {
+        adMoney.text = "AD Money: " + AdMoney;
+    }
+
+    public void ShowAd()
+    {
+        if (Advertisement.IsReady())
+        {
+            Advertisement.Show("rewardedVideo", new ShowOptions() { resultCallback = HandleAdResult });
+        }
+    }
+
+    public void HandleAdResult(ShowResult result)
+    {
+        switch (result)
+        {
+            case ShowResult.Finished:
+                AdMoney++;
+                PlayerPrefs.SetInt("AdMoney", AdMoney);
+                break;
+            case ShowResult.Skipped:
+                Debug.Log("Player skipped ad");
+                break;
+            case ShowResult.Failed:
+                Debug.Log("Player failed to load ad");
+                break;
+        }
+    }
 }
-//2685945
