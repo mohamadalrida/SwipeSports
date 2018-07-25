438a692cf9fad37c68aae7a788549c544df5b839
AminBahsa <37161900+AminBahsa@users.noreply.github.com>
2018-07-25 15:57:20 +1000

    audio files added

---
+++ T5Mobile/Assets/Scripts/CrowdShake.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrowdShake : MonoBehaviour {
+
+    public float shakeSpeed;
+    public float shakeDistance;
+
+    private Vector2 defaultPos;
+
+    Rigidbody2D rb;
+
+    public float gravityScale;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.position = defaultPos + Random.insideUnitCircle*shakeSpeed;
+
+        rb.gravityScale = gravityScale;
+
+
+    }
+}
