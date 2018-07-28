4643b8d861d3fe309ab17bb113132ac13cc1cd7b
mohamadalrida <36973080+mohamadalrida@users.noreply.github.com>
2018-07-28 22:02:25 +1000

    Adjusted end level with tag check

--- T5Mobile/Assets/Scripts/SoccerWinLevel.cs
+++ T5Mobile/Assets/Scripts/SoccerWinLevel.cs
@@ -38,10 +38,13 @@ public class SoccerWinLevel : MonoBehaviour {
 
     private void OnTriggerEnter2D(Collider2D Ball)
     {
-        gameObject.GetComponent<BoxCollider2D>().enabled = false;       //turn off collider
-        SoccerManager.yourGoals++;                                      //adds 1 goal to your score and starts 2 functions
-        StartCoroutine(FinishLevel());
-        AddToTable();
+        if (Ball.tag == "Player")
+        {
+            gameObject.GetComponent<BoxCollider2D>().enabled = false;       //turn off collider
+            SoccerManager.yourGoals++;                                      //adds 1 goal to your score and starts 2 functions
+            StartCoroutine(FinishLevel());
+            AddToTable();
+        }
     }
 
     IEnumerator FinishLevel()
