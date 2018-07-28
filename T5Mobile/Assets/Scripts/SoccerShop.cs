9f80499a67ae6e50490bec0da880dee72df4aa05
mohamadalrida <36973080+mohamadalrida@users.noreply.github.com>
2018-07-28 21:57:11 +1000

    Shop fixed

--- T5Mobile/Assets/Scripts/SoccerShop.cs
+++ T5Mobile/Assets/Scripts/SoccerShop.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Shop : MonoBehaviour {
+public class SoccerShop : MonoBehaviour {
 
     public int Cost1;
     public int Cost2;
@@ -21,82 +21,61 @@ public class Shop : MonoBehaviour {
     public Button btn6;
     public Button btnAD;
 
-    public static int Purchased1;
-    public static int Purchased2;
-    public static int Purchased3;
-    public static int Purchased4;
-    public static int Purchased5;
-    public static int Purchased6;
-    public static int PurchasedAD;
+    public static int SPurchased1;
+    public static int SPurchased2;
+    public static int SPurchased3;
+    public static int SPurchased4;
+    public static int SPurchased5;
+    public static int SPurchased6;
+    public static int SPurchasedAD;
 
-    public GameObject nextShopSB;
-    public GameObject nextShopGB;
-    public GameObject shopGB;
-    public GameObject shopSB;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        SPurchased1 = PlayerPrefs.GetInt("SPurchased1", SPurchased1);
+        SPurchased2 = PlayerPrefs.GetInt("SPurchased2", SPurchased2);
+        SPurchased3 = PlayerPrefs.GetInt("SPurchased3", SPurchased3);
+        SPurchased4 = PlayerPrefs.GetInt("SPurchased4", SPurchased4);
+        SPurchased5 = PlayerPrefs.GetInt("SPurchased5", SPurchased5);
+        SPurchased6 = PlayerPrefs.GetInt("SPurchased6", SPurchased6);
+        SPurchasedAD = PlayerPrefs.GetInt("SPurchasedAD", SPurchasedAD);
 
-    private void Update()
-    {
-        Purchased1 = PlayerPrefs.GetInt("Purchased1", Purchased1);
-        Purchased2 = PlayerPrefs.GetInt("Purchased2", Purchased2);
-        Purchased3 = PlayerPrefs.GetInt("Purchased3", Purchased3);
-        Purchased4 = PlayerPrefs.GetInt("Purchased4", Purchased4);
-        Purchased5 = PlayerPrefs.GetInt("Purchased5", Purchased5);
-        Purchased6 = PlayerPrefs.GetInt("Purchased6", Purchased6);
-        PurchasedAD = PlayerPrefs.GetInt("PurchasedAD", PurchasedAD);
-
-        if (CoinManager.Coins < Cost1 || Purchased1 == 1)
+        if (CoinManager.Coins < Cost1 || SPurchased1 == 1)
         {
             btn1.interactable = false;
         }
-        //else if (CoinManager.Coins >= Cost1)
-        //{
-        ////    btn1.interactable = true;
-        //}
 
-        if (CoinManager.Coins < Cost2 || Purchased2 == 1)
+        if (CoinManager.Coins < Cost2 || SPurchased2 == 1)
         {
             btn2.interactable = false;
         }
 
-        if (CoinManager.Coins < Cost3 || Purchased3 == 1)
+        if (CoinManager.Coins < Cost3 || SPurchased3 == 1)
         {
             btn3.interactable = false;
         }
 
-        if (CoinManager.Coins < Cost4 || Purchased4 == 1)
+        if (CoinManager.Coins < Cost4 || SPurchased4 == 1)
         {
             btn4.interactable = false;
         }
 
-        if (CoinManager.Coins < Cost5 || Purchased5 == 1)
+        if (CoinManager.Coins < Cost5 || SPurchased5 == 1)
         {
             btn5.interactable = false;
         }
-        if (CoinManager.Coins < Cost6 || Purchased6 == 1)
+        if (CoinManager.Coins < Cost6 || SPurchased6 == 1)
         {
             btn6.interactable = false;
         }
-        if(ADMoney.AdMoney < CostAD || PurchasedAD == 1)
+        if (ADMoney.AdMoney < CostAD || SPurchasedAD == 1)
         {
             btnAD.interactable = false;
         }
-
-    }
-
-    public void NextShopSB()
-    {
-        nextShopSB.SetActive(false);
-        nextShopGB.SetActive(true);
-        shopGB.SetActive(false);
-        shopSB.SetActive(true);
-    }
-
-    public void NextShopGB()
-    {
-        nextShopSB.SetActive(true);
-        nextShopGB.SetActive(false);
-        shopGB.SetActive(true);
-        shopSB.SetActive(false);
     }
 
     public void Purchase1()
@@ -106,12 +85,12 @@ public class Shop : MonoBehaviour {
             CoinManager.Coins -= Cost1;
             PlayerPrefs.SetInt("Coins", CoinManager.Coins);
 
-            Purchased1 = 1;
-            PlayerPrefs.SetInt("Purchased1", Purchased1);
+            SPurchased1 = 1;
+            PlayerPrefs.SetInt("Purchased1", SPurchased1);
 
-            
+
         }
-        
+
     }
 
     public void Purchase2()
@@ -124,8 +103,8 @@ public class Shop : MonoBehaviour {
             PlayerPrefs.SetInt("Coins", CoinManager.Coins);
             Debug.Log("purchased");
 
-            Purchased2 = 1;
-            PlayerPrefs.SetInt("Purchased2", Purchased2);
+            SPurchased2 = 1;
+            PlayerPrefs.SetInt("Purchased2", SPurchased2);
 
         }
     }
@@ -139,8 +118,8 @@ public class Shop : MonoBehaviour {
             PlayerPrefs.SetInt("Coins", CoinManager.Coins);
             Debug.Log("purchased");
 
-            Purchased3= 1;
-            PlayerPrefs.SetInt("Purchased3", Purchased3);
+            SPurchased3 = 1;
+            PlayerPrefs.SetInt("Purchased3", SPurchased3);
         }
     }
 
@@ -153,8 +132,8 @@ public class Shop : MonoBehaviour {
             PlayerPrefs.SetInt("Coins", CoinManager.Coins);
             Debug.Log("purchased");
 
-            Purchased4= 1;
-            PlayerPrefs.SetInt("Purchased4", Purchased4);
+            SPurchased4 = 1;
+            PlayerPrefs.SetInt("Purchased4", SPurchased4);
         }
     }
 
@@ -167,8 +146,8 @@ public class Shop : MonoBehaviour {
             PlayerPrefs.SetInt("Coins", CoinManager.Coins);
             Debug.Log("purchased");
 
-            Purchased5 = 1;
-            PlayerPrefs.SetInt("Purchased5", Purchased5);
+            SPurchased5 = 1;
+            PlayerPrefs.SetInt("Purchased5", SPurchased5);
         }
     }
 
@@ -181,8 +160,8 @@ public class Shop : MonoBehaviour {
             PlayerPrefs.SetInt("Coins", CoinManager.Coins);
             Debug.Log("purchased");
 
-            Purchased6 = 1;
-            PlayerPrefs.SetInt("Purchased6", Purchased6);
+            SPurchased6 = 1;
+            PlayerPrefs.SetInt("Purchased6", SPurchased6);
         }
     }
 
@@ -195,8 +174,8 @@ public class Shop : MonoBehaviour {
             PlayerPrefs.SetInt("AdMoney", ADMoney.AdMoney);
             Debug.Log("purchased");
 
-            PurchasedAD = 1;
-            PlayerPrefs.SetInt("PurchasedAD", PurchasedAD);
+            SPurchasedAD = 1;
+            PlayerPrefs.SetInt("SPurchasedAD", SPurchasedAD);
         }
     }
 
