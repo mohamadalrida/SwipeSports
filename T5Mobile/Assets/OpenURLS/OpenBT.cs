23de3b87e39bd0351e85300a0afbf9f50567aabe
mohamadalrida <36973080+mohamadalrida@users.noreply.github.com>
2018-08-02 09:38:00 +1000

    Added credits and ad coin

---
+++ T5Mobile/Assets/OpenURLS/OpenBT.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenBT : MonoBehaviour {
+
+	public void OpenBigThink()
+    {
+        Application.OpenURL("https://www.facebook.com/BigThinkStudios/");
+    }
+}
+
---
+++ T5Mobile/Assets/OpenURLS/OpenVOIDFALL.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OpenVOIDFALL : MonoBehaviour {
+
+	public void OpenVF()
+    {
+        Application.OpenURL("https://play.google.com/store/apps/details?id=com.BigThinkStudios.VoidFall");
+    }
+}
