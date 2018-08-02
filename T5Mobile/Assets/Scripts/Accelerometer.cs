59ca9e01d061f3da08f51e412344899241290115
AminBahsa <37161900+AminBahsa@users.noreply.github.com>
2018-08-02 12:15:05 +1000

    Aceelerometer Clamp added

--- T5Mobile/Assets/Scripts/Accelerometer.cs
+++ T5Mobile/Assets/Scripts/Accelerometer.cs
@@ -4,21 +4,36 @@ using UnityEngine;
 
 public class Accelerometer : MonoBehaviour {
 
-    public float xMin;
-    public float xMax;
-    public float yMin;
-    public float yMax;
+    // Disable This If Doesnt Work
+    // Disble This If Doesnt Work
+    public Vector2 minPosition = new Vector2(-7,-4);
+    public Vector2 maxPosition = new Vector2(7.5f, 4);
+    // Disable This If Doesnt Work
+    // Disable This If Doesnt Work
 
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
+
         transform.Translate(Input.acceleration.x, Input.acceleration.y, 0);
+
+
+        // Disable This If Doesnt Work
+        // Disable This If Doesnt Work
+        // get position 
+        Vector2 Pos = transform.position;
+
+        // clamp the x coordinate of Pos 
+        Pos.x = Mathf.Clamp(Pos.x, minPosition.x, maxPosition.x);
+        Pos.y = Mathf.Clamp(Pos.y, minPosition.y, maxPosition.y);
+        // Disable This If Doesnt Work
+        // Disable This If Doesnt Work
 
         //transform.position = new Vector3(Mathf.Clamp(Time.time, xMin, xMax), (Mathf.Clamp(Time.time, yMin, yMax)), 0);
     }
