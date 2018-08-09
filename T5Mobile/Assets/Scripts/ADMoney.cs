d91a3ac022b1f24bdd67f70b0ee940c3dc3a5371
mohamadalrida <36973080+mohamadalrida@users.noreply.github.com>
2018-08-09 11:54:09 +1000

    ADS fixed

--- T5Mobile/Assets/Scripts/ADMoney.cs
+++ T5Mobile/Assets/Scripts/ADMoney.cs
@@ -12,18 +12,21 @@ public class ADMoney : MonoBehaviour {
     public bool android;
     public bool ios;
 
+    public GameObject panel;
+    public TextMeshProUGUI moneyEarned;
+
 
 	// Use this for initialization
 	void Start () {
 
         if (android == true)
         {
-            Advertisement.Initialize("2685945");
+            Advertisement.Initialize("2726910");
         }
 
         if (ios == true)
         {
-            Advertisement.Initialize("2685944");
+            Advertisement.Initialize("2726909");
         }
         //Advertisement.Initialize("2685945");
 
@@ -39,7 +42,7 @@ public class ADMoney : MonoBehaviour {
     {
         if (Advertisement.IsReady())
         {
-            Advertisement.Show("rewardedVideo", new ShowOptions() { resultCallback = HandleAdResult });
+            Advertisement.Show("", new ShowOptions() { resultCallback = HandleAdResult });
         }
     }
 
@@ -48,15 +51,35 @@ public class ADMoney : MonoBehaviour {
         switch (result)
         {
             case ShowResult.Finished:
-                AdMoney++;
+                int x = Random.Range(2, 4);
+                AdMoney += x;
                 PlayerPrefs.SetInt("AdMoney", AdMoney);
+
+                panel.SetActive(true);
+                moneyEarned.fontSize = 64;
+                moneyEarned.text = "Thanks for watching the Ad! You've earned " + x + " AD coins! Thanks for not skipping the Ad.";
+                
                 break;
             case ShowResult.Skipped:
+                AdMoney++;
                 Debug.Log("Player skipped ad");
+
+                panel.SetActive(true);
+                moneyEarned.fontSize = 62; ;
+                moneyEarned.text = "Thanks for watching the Ad! You've earned 1 AD coin, although you could get more if you watch the full Ad.";
                 break;
             case ShowResult.Failed:
+
+                panel.SetActive(true);
+                moneyEarned.fontSize = 64;
+                moneyEarned.text = "AD failed to load. Please check that you are connected to the internet and try again.";
                 Debug.Log("Player failed to load ad");
                 break;
         }
+    }
+
+    public void Continue()
+    {
+        panel.SetActive(false);
     }
 }
