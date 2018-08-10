07d82f8f568c9bc61b22678a99db4b7b9f7d7b0e
mohamadalrida <36973080+mohamadalrida@users.noreply.github.com>
2018-08-10 20:34:38 +1000

    iOS boolean and link

--- T5Mobile/Assets/OpenURLS/OpenVOIDFALL.cs
+++ T5Mobile/Assets/OpenURLS/OpenVOIDFALL.cs
@@ -5,8 +5,20 @@ using UnityEngine.UI;
 
 public class OpenVOIDFALL : MonoBehaviour {
 
+    public bool Android;
+    public bool iOS;
+
 	public void OpenVF()
     {
-        Application.OpenURL("https://play.google.com/store/apps/details?id=com.BigThinkStudios.VoidFall1");
+        if (Android == true)
+        {
+            Application.OpenURL("https://play.google.com/store/apps/details?id=com.BigThinkStudios.VoidFall1");
+        }
+        else if (iOS == true)
+        {
+            Application.OpenURL("https://itunes.apple.com/au/app/void-fall/id1417630235?mt=8");
+        }
     }
+
+    
 }
